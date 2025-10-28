12d43216fc4b38bd75e9def49775b1cb5f78bfd7
Vincent <vincentnator2002@gmail.com>
2025-10-29 05:34:23 +0800

    bg.png

--- src/components/lesson/PersonalHygieneLevel1.js
+++ src/components/lesson/PersonalHygieneLevel1.js
@@ -24,7 +24,7 @@ import {
 
 // Images
 // import successGif from "../../assets/hygienelevel1/roblox.gif"
-import bathroomBg from "../../assets/hygienelevel1/bg-copy.png"
+import bathroomBg from "../../assets/hygienelevel1/bg.png"
 import sinkImg from "../../assets/hygienelevel1/sink.png"
 import faucetImg from "../../assets/hygienelevel1/onfaucet.png"
 import leftHandImg from "../../assets/hygienelevel1/lefthand.png"
