238d4b427bac02340a10d3de6c166e0e7660587f
Vincent <vincentnator2002@gmail.com>
2025-10-29 09:55:13 +0800

    type role and id corrections

--- src/components/lesson/CookingLevel1.js
+++ src/components/lesson/CookingLevel1.js
@@ -93,10 +93,10 @@ export default function FriedEggLevel1() {
   // Get student ID from localStorage
   const getStudentId = () => {
     const studentId = localStorage.getItem('studentId');
-    const userType = localStorage.getItem('userType');
+    const userRole = localStorage.getItem('userRole');
     
-    if (userType !== 'STUDENT') {
-      console.error('User is not a student:', userType);
+    if (userRole !== 'STUDENT') {
+      console.error('User is not a student:', userRole);
       return null;
     }
     
